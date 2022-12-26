8453fcf80941e284666f36628545e123dd281b9b
shokhrukhKarimov <102215837+shokhrukh1212@users.noreply.github.com>
2022-12-25 22:52:49 -0600

    Add files via upload

---
+++ SPA/pages/home.html
@@ -0,0 +1,1 @@
+<h1>Welcome to HOME page</h1>
---
+++ SPA/router/router.js
@@ -0,0 +1,62 @@
+window.onload = () => {
+    const path = window.location.pathname.split('/');
+
+    switch(path[1]) 
+    {
+        case "":
+        {
+            loadPage('home');
+            break;
+        }
+        case "about":
+        {
+            loadPage('about');
+            break;
+        }
+        case "pricing":
+        {
+            loadPage('pricing');
+            break;
+        }
+        default:
+        {
+            loadPage('404');
+            break;
+        }
+    }
+
+
+    document.querySelectorAll('.menu__item').forEach(item => {
+        item.addEventListener('click', () => {
+            const path = item.getAttribute('value');
+            loadPage(path);
+
+
+            //history API
+            if(path == 'home') {
+                window.history.pushState("", "", "/");
+                return;
+            }
+
+            window.history.pushState("", "", path)
+        })
+    })
+
+
+    function loadPage($path) {
+        if($path == '') return;
+
+        const container = document.getElementById('container')
+        const request = new XMLHttpRequest();
+        request.open('GET', $path + '../.html');
+        request.send();
+
+        request.onload = () => { // this works when data comes from a server
+            if(request.status == 200) 
+            {
+                container.innerHTML = request.responseText;
+            }
+        }
+
+    }
+}
